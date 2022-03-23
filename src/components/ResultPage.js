bf7c05ba4c75762b8f9bb1955c51bdfb4d787e79
Akshata Khedekar <akshatapkhedekar01@gmail.com>
2022-03-23 21:27:50 +0530

    result page styling

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -2,31 +2,35 @@ import React, { useState, useEffect } from "react"
 import { useHistory } from "react-router-dom";
 import ReactPaginate from 'react-paginate';
 import '../App.css'
+import { isEmpty } from 'lodash';
 
 function ResultPage() {
     let history = useHistory();
     const { location } = history;
     const { state } = location;
-    const [response, setResponse] = useState(state.data);
-    const [currentPage, setCurrentPage] = useState(0);
+    console.log(state)
+    const [pageOffset, setPageOffset] = useState(1);
+    const [news, setNews] = useState(state.data);
 
-    const getNews = (pageCount) => {
+    const handlePageChange = (event) => {
+        setPageOffset(event.selected + 1);
+    };
 
-        const result = fetch(`http://content.guardianapis.com/search?api-key=test&q=${state.searchTerm}&show-fields=thumbnail,headline&show-tags=keyword&page=${pageCount
-            }&page-size=10`)
+    useEffect(() => {
+        fetch(`http://content.guardianapis.com/search?api-key=test&q=${state.searchTerm}&show-fields=thumbnail,headline&show-tags=keyword&page=${pageOffset}&page-size=10`)
             .then(data => data.json())
-            .then(data => {
-                console.log("search", data)
-                setResponse(data)
+            .then((data) => {
+                setNews(data.response);
             });
-    }
 
-    console.log("response", response)
+    }, [pageOffset]);
+
+
     return (
         <div className="CentralizationNews">
             <h2 style={{ color: "#000" }}>Search Results</h2>
-            {response.results.length > 0 && response.results.map((item) => {
-                console.log("item", item);
+            {news && news.results.map((item) => {
+                // console.log("item", item);
                 return (
                     <div className="newsContainer" key={item.id}>
                         <h3>{item.fields.headline}</h3>
@@ -38,22 +42,28 @@ function ResultPage() {
                     </div>
                 )
             })}
-            {/* <span
-            // onClick={(e) => getNews((Number(response.currentPage) < 0 ? 0 : Number(response.currentPage)) - 10)}
-            >
-                Prev
-            </span>
-            {response.results.length > 0 && response.results.map((item, index) => {
-                console.log("item", item);
-                return (
-                    <span style={{ color: "#000", paddingRight: "5px" }} key={item.id + "page"}>{index}</span>
-                )
-            })}
-            <span
-            // onClick={(e) => getNews(Number(response.currentPage) + 10)}
-            >
-                Next
-            </span> */}
+
+            <ReactPaginate
+                previousLabel="Previous"
+                nextLabel="Next"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                pageCount={10}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageChange}
+                containerClassName="pagination"
+                activeClassName="active"
+
+            />
+
         </div>
     )
 }
